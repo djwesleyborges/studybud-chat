6364df58fa463eab9f7195b09dc44f0f44f638e3
Wesley Borges <djwesleyborges@gmail.com>
2023-07-29 10:30:51 -0300

    feat: crud using forms

--- base/models.py
+++ base/models.py
@@ -20,6 +20,9 @@ class Room(models.Model):
     # auto_now_add never change the value
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return str(self.name)
 
--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from .models import Room
-
-from django.shortcuts import render
+from .forms import RoomForm
+from django.shortcuts import render, redirect
 
 rooms = [
     {'id': 1, 'name': 'Lets learn python!'},
@@ -19,3 +19,35 @@ def room(request, pk):
     rooms = Room.objects.get(id=pk)
     context = {'room': rooms}
     return render(request=request, template_name='base/room.html', context=context)
+
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request=request, template_name='base/room_form.html', context=context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(data=request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request=request, template_name='base/room_form.html', context=context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        room.save()
+        return redirect('home')
+    return render(request=request, template_name='base/delete.html', context={'obj': room})
