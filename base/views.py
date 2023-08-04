3ff418441f7aa1310abb2925af0295c8a8e4dfde
Wesley Borges <djwesleyborges@gmail.com>
2023-08-04 15:20:11 -0300

    feat: implemented template to frontend

--- base/views.py
+++ base/views.py
@@ -103,19 +103,24 @@ def user_profile(request, pk):
 @login_required(login_url='login')
 def create_room(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(data=request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
+        return redirect('home')
+    context = {'form': form, 'topics': topics}
     return render(request=request, template_name='base/room_form.html', context=context)
 
 
 @login_required(login_url='login')
 def update_room(request, pk):
+    topics = Topic.objects.all()
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
 
@@ -123,11 +128,14 @@ def update_room(request, pk):
         return HttpResponse('Your are not allowed here!!!')
 
     if request.method == 'POST':
-        form = RoomForm(data=request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+    context = {'form': form, 'topics': topics}
     return render(request=request, template_name='base/room_form.html', context=context)
 
 
